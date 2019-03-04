f538dd398e4b17835b9242727bed9d45a10cd853
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2019-03-04 16:51:27 +0200

    Updates to Window and Game

--- src/Main.java
+++ src/Main.java
@@ -18,14 +18,16 @@ public class Main {
         //Spaceship x = new Spaceship(100);
         //.out.println("Mass of spaceship x is " + x.getMass());
 
-        //MassObject[] planets = {
-        //    new MassObject(6e24, 0, 0),
-        //    new MassObject(10, 0, 6378000)
-        //};
+        MassObject[] planets = {
+            new MassObject(6e24, 0, 0),
+            new Spaceship(10, 0, 6378000)
+        };
+        /*
         MassObject[] planets = new MassObject[10];
         for(int i = 0; i < planets.length; i++){
             planets[i] = new MassObject(randomWithRange(1e9d, 9e10d), randomWithRange(-9e8d, 9e8d), randomWithRange(-9e8d, 9e10d));
         }
+        */
         for(int i = 0; i < planets.length; i++){
             planets[i].calculateGravity();
         }
